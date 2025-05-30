f6cfe74d19561f12029e1d91674788abe6849602
ad-dw <aditya_dwivedi01@outlook.com>
2025-05-30 16:07:17 +0530

    Integrating google popup sign in using firebase.

--- src/routes/SignIn/SignIn.component.jsx
+++ src/routes/SignIn/SignIn.component.jsx
@@ -1,7 +1,20 @@
 import "./signIn.styles.scss";
+import {
+  signInWithGooglePopup,
+  createUserDocumentFromAuth,
+} from "../../utils/Firebase/firebase.utils";
 
 const SignIn = () => {
-  return <div>I'm a sign in page.</div>;
+  const signInGoogleUser = async () => {
+    let { user } = await signInWithGooglePopup();
+    const userAuthRef = await createUserDocumentFromAuth(user);
+  };
+  return (
+    <div>
+      I'm a sign in page.
+      <button onClick={signInGoogleUser}>Sign in with google popup</button>
+    </div>
+  );
 };
 
 export default SignIn;
---
+++ src/utils/Firebase/firebase.utils.js
@@ -0,0 +1,46 @@
+import { initializeApp } from "firebase/app";
+
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBCDO313fRGEbw5UM5pTbgk6sjBDEiuZjs",
+  authDomain: "crown-clothing-db-e40ff.firebaseapp.com",
+  projectId: "crown-clothing-db-e40ff",
+  storageBucket: "crown-clothing-db-e40ff.firebasestorage.app",
+  messagingSenderId: "375468360998",
+  appId: "1:375468360998:web:5372c7d9912a32b56d2de7",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({ prompt: "select_account" });
+
+export const auth = getAuth();
+
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+  const userSnapshot = await getDoc(userDocRef);
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    setDoc(userDocRef, {
+      displayName,
+      email,
+      createdAt,
+    });
+  }
+  return userDocRef;
+};
