40539ce3b594e2cf72ec146efef9c22c93308e10
ad-dw <aditya_dwivedi01@outlook.com>
2025-07-13 16:36:04 +0530

    Cleaning-up-code

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.scss";
 import App from "./App.jsx";
 import { CartContextProvider } from "./context/Cart.context.jsx";
-// import { CategoriesContextProvider } from "./context/Categories.context.jsx";
 import { Provider } from "react-redux";
 import { reduxStore } from "./reduxStore/reduxStore.js";
 
@@ -12,11 +11,9 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Provider store={reduxStore}>
-        {/* <CategoriesContextProvider> */}
         <CartContextProvider>
           <App />
         </CartContextProvider>
-        {/* </CategoriesContextProvider> */}
       </Provider>
     </BrowserRouter>
   </StrictMode>
