ec023fc85f945a7d6ba0cc3e2231585ab918a9f6
ad-dw <aditya_dwivedi01@outlook.com>
2025-07-15 21:40:17 +0530

    Adding cart reducer

--- src/components/CategoryView/CategoryView.component.jsx
+++ src/components/CategoryView/CategoryView.component.jsx
@@ -3,16 +3,18 @@ import ProductCard from "../ProductCard/ProductCard.component.jsx";
 
 const CategoryView = ({ title, products }) => {
   return (
-    <div className="category-view-container">
-      <h2>
-        <span className="category-title">{title.toUpperCase()}</span>
-      </h2>
-      <div className="view">
-        {products.map((product) => (
-          <ProductCard product={product} key={product.id} />
-        ))}
+    products && (
+      <div className="category-view-container">
+        <h2>
+          <span className="category-title">{title.toUpperCase()}</span>
+        </h2>
+        <div className="view">
+          {products.map((product) => (
+            <ProductCard product={product} key={product.id} />
+          ))}
+        </div>
       </div>
-    </div>
+    )
   );
 };
 
