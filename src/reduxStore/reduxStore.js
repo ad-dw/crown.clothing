92190dc6afca0c50682b107e2ae99c35a85610d8
ad-dw <aditya_dwivedi01@outlook.com>
2025-07-19 19:41:15 +0530

    Added redux persist library

--- src/reduxStore/reduxStore.js
+++ src/reduxStore/reduxStore.js
@@ -3,13 +3,24 @@ import {
   compose,
   legacy_createStore as createStore,
 } from "redux";
+
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import { rootReducer } from "./rootReducer";
 import { logger } from "redux-logger";
 
 const middlewares = [logger];
+const persistConfig = {
+  key: "root",
+  storage,
+  blacklist: ["user"],
+};
 const composedEnhancers = compose(applyMiddleware(...middlewares));
+const enhancedReducer = persistReducer(persistConfig, rootReducer);
 export const reduxStore = createStore(
-  rootReducer,
+  enhancedReducer,
   undefined,
   composedEnhancers
 );
+
+export const persistor = persistStore(reduxStore);
